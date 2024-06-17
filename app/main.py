d34064b58e9a7b48b5d1d680e9540bf6b510025b
Illia <illiatsiuper@gmail.com>
2024-06-17 16:05:26 +0300

    Fixing requested changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from typing import List, Dict, Any
+from typing import Any
 
 
-def format_linter_error(error: Dict[str, Any]) -> Dict[str, Any]:
+def format_linter_error(error: list[dict]) -> dict[str, Any]:
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -13,7 +13,7 @@ def format_linter_error(error: Dict[str, Any]) -> Dict[str, Any]:
 
 def format_single_linter_file(
         file_path: str,
-        errors: List[Dict[str, Any]]) -> Dict[str, Any]:
+        errors: list[dict]) -> dict[str, Any]:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
@@ -21,7 +21,6 @@ def format_single_linter_file(
     }
 
 
-def format_linter_report(
-        linter_report: Dict[str, Any]) -> List[Dict[str, Any]]:
+def format_linter_report(linter_report: list[dict]) -> list[dict[str, Any]]:
     return [format_single_linter_file(file_path, errors)
             for file_path, errors in linter_report.items()]
